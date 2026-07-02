54b39cd754344b29df6aa27dadb51b0d2d01e320
takwamohammed691-lab <takwamohammed691@gmail.com>
2026-07-02 23:09:58 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const button = document.getElementById("helloBtn");
+
+button.addEventListener("click", () => {
+
+button.innerHTML = "💖 Hello!";
+
+button.style.background = "#8cf2b5";
+
+document.body.style.cursor = "pointer";
+
+});
